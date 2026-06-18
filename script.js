43d8e3752f79949a56bc56cbae05ad6e4e87cf98
Mk designs <98295427+Dhichu@users.noreply.github.com>
2026-06-18 19:22:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Jewellery Website Loaded");
+
+const rates = {
+gold22: "₹8,520 / gm",
+gold24: "₹9,290 / gm",
+silver: "₹118 / gm"
+};
+
+document.getElementById("gold22").innerText = rates.gold22;
+document.getElementById("gold24").innerText = rates.gold24;
+document.getElementById("silver").innerText = rates.silver;
